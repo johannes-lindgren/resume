5a67038dc53be155b4c42b70543b1b02a7091ae6
johannes-lindgren <johannes-lindgren@hotmail.com>
2023-01-26 15:04:49 +0100

    Improved theme

--- src/design/lightTheme.ts
+++ src/design/lightTheme.ts
@@ -3,5 +3,31 @@ import { createTheme } from '@mui/material'
 export const lightTheme = createTheme({
   palette: {
     mode: 'light',
+    primary: {
+      main: '#D1462F',
+    },
+    secondary: {
+      main: '#C8963E',
+    },
+  },
+  typography: {
+    h1: {
+      fontSize: '1.75rem',
+    },
+    h2: {
+      fontSize: '1.5rem',
+    },
+    h3: {
+      fontSize: '1.25rem',
+    },
+    h4: {
+      fontSize: '1.125rem',
+    },
+    h5: {
+      fontSize: '1rem',
+    },
+    h6: {
+      fontSize: '0.8rem',
+    },
   },
 })
