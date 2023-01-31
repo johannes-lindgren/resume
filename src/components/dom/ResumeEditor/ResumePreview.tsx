730b1cdab10bf39ce238c9afbf33e399dd19757b
johannes-lindgren <johannes-lindgren@hotmail.com>
2023-01-31 18:27:41 +0100

    Fixed aspect ratio for preview iframe

--- src/components/dom/ResumeEditor/ResumePreview.tsx
+++ src/components/dom/ResumeEditor/ResumePreview.tsx
@@ -1,4 +1,13 @@
-import { FunctionComponent, useCallback, useMemo, useState } from 'react'
+import {
+  FunctionComponent,
+  ReactNode,
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react'
 import { Resume } from '@/model/resume'
 import { Setter } from '@/utils/Setter'
 import { useThrottledState } from '@/hooks/useThrottledState'
@@ -39,11 +48,14 @@ const PdfRoot = styled(PdfDocument)(({ theme }) => ({
   aspectRatio: '0.707107 / 1',
   border: 'none',
   overflow: 'hidden',
+  width: '100%',
+  height: '100%',
   borderRadius: theme.shape.borderRadius * 2,
 }))
 
 const PreviewToolbar = styled(Box)(({ theme }) => ({
   display: 'flex',
+  flexWrap: 'wrap',
   gap: theme.spacing(1),
 }))
 
@@ -62,29 +74,119 @@ export const ResumePreview: FunctionComponent<{
   )
 
   return (
-    <>
-      <PreviewToolbar justifyContent="left">
-        <SaveStatusBox isSaved={props.isSaved} />
-        <Box flex={1} />
-        <DeleteButton
-          removeResume={props.removeResume}
-          resume={resume}
-        />
-        <UploadResumeButton
-          color="inherit"
-          onChange={props.setResume}
-        />
-        <DownloadResumeButton
-          color="inherit"
-          resume={resume}
-          suggestedName={`${resume.name}'s resumé`}
-        />
-      </PreviewToolbar>
+    <PreviewLayout
+      header={
+        <PreviewToolbar justifyContent="left">
+          <SaveStatusBox isSaved={props.isSaved} />
+          <Box flex={1} />
+          <DeleteButton
+            removeResume={props.removeResume}
+            resume={resume}
+          />
+          <UploadResumeButton
+            color="inherit"
+            onChange={props.setResume}
+          />
+          <DownloadResumeButton
+            color="inherit"
+            resume={resume}
+            suggestedName={`${resume.name}'s resumé`}
+          />
+        </PreviewToolbar>
+      }
+      footer={
+        <PreviewToolbar justifyContent="right">
+          <DownloadPdfButton document={doc} />
+        </PreviewToolbar>
+      }
+    >
       <PdfRoot showToolbar={false}>{doc}</PdfRoot>
-      <PreviewToolbar justifyContent="right">
-        <DownloadPdfButton document={doc} />
-      </PreviewToolbar>
-    </>
+    </PreviewLayout>
+  )
+}
+
+const PreviewLayout: FunctionComponent<{
+  children?: ReactNode
+  header?: ReactNode
+  footer?: ReactNode
+}> = (props) => {
+  type Dimension = {
+    width: number
+    height: number
+  }
+  const ref = useRef<HTMLDivElement>(null)
+  const [dim, setDim] = useState<Dimension>({
+    width: 0,
+    height: 0,
+  })
+  useLayoutEffect(() => {
+    if (!ref.current) {
+      return
+    }
+    const element = ref.current
+    const handleResize = () => {
+      const { clientWidth, clientHeight } = element
+      console.log(clientWidth, clientHeight)
+      const aspectRatio = 1.4142135624
+      const aspectRatioInv = 0.7071067812
+      const v1: Dimension = {
+        width: clientWidth,
+        height: clientWidth * aspectRatio,
+      }
+      const v2: Dimension = {
+        width: clientHeight * aspectRatioInv,
+        height: clientHeight,
+      }
+      console.log('v1', v1)
+      console.log('v2', v2)
+      setDim(v1.height > clientHeight ? v2 : v1)
+    }
+    window.addEventListener('resize', handleResize)
+    const observer = new MutationObserver(handleResize)
+    observer.observe(window.document, {
+      attributes: true,
+      childList: true,
+      characterData: true,
+      subtree: true,
+    })
+    return () => {
+      window.removeEventListener('resize', handleResize)
+      observer.disconnect()
+    }
+  }, [setDim])
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 2,
+      }}
+    >
+      <Box width={dim.width}>{props.header}</Box>
+      <Box
+        ref={ref}
+        sx={{
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Box
+          width={dim.width}
+          height={dim.height}
+        >
+          {props.children}
+        </Box>
+      </Box>
+      <Box width={dim.width}>{props.footer}</Box>
+    </Box>
   )
 }
 
