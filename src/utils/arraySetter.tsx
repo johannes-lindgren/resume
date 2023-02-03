23358744ef86144894ecb0695247e19bab58b1b6
johannes-lindgren <johannes-lindgren@hotmail.com>
2023-02-03 15:31:26 +0100

    Removed unused old version of arraySetter

--- src/utils/arraySetter.tsx
+++ src/utils/arraySetter.tsx
@@ -1,21 +1,5 @@
 import { replaced } from '@/utils/replaced'
-import { Setter, Setter2 } from '@/utils/Setter'
-
-export const arraySetter =
-  <
-    Key extends string,
-    Child extends { uid: string },
-    Parent extends Record<Key, Child[]>,
-  >(
-    parent: Parent,
-    setParent: Setter<Parent>,
-    key: Key,
-  ): Setter<Child> =>
-  (newChild: Child) =>
-    setParent({
-      ...parent,
-      [key]: replaced(parent[key], (it) => it.uid === newChild.uid, newChild),
-    })
+import { Setter2 } from '@/utils/Setter'
 
 export const arraySetter2 =
   <
